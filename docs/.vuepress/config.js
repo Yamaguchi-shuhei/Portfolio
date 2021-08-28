7472d6354ba44af75550de7510dd376fa3149f31
Yamaguchi-shuhei <81962317+Yamaguchi-shuhei@users.noreply.github.com>
2021-08-28 19:03:45 +0900

    ヘッダーを作った

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about.html' },
+        { text: 'Blog', link: 'https://example.com/' }
+      ]
+    }
+  }
